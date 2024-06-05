caf810400608aa57c806c8fa3b109080f29ac508
ne-supanat <ne-supanat@users.noreply.github.com>
2024-06-05 05:25:10 +0000

    deploy: 67265e4087a549b48610a5a6e915450d656bb025

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,11 +4,11 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "ff17683e2d82b343490f2da6625704ec",
-"index.html": "35263262bdc0650290fd2fdb1adf6655",
-"/": "35263262bdc0650290fd2fdb1adf6655",
+"index.html": "ec60f9975c5d1467ddc4fe1c37e6687b",
+"/": "ec60f9975c5d1467ddc4fe1c37e6687b",
 "main.dart.js": "7f90248e24f2414ae7478635f4ee5ea6",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"favicon.png": "694938baf107878e28cbd658bb828c11",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
